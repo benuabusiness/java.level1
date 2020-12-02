705283a1236c63f832e879270d7f504b9854c647
benuacontent <benuacontent@gmail.com>
2020-12-03 00:58:56 +0400

    Создал переменные всех пройденных типов данных, и инициализировать их значения;

---
+++ Задание2.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.homework1;
+
+public class main {
+    public static void main(String[] args){
+        byte b = -11;
+        short s = 516;
+        int i = 999111;
+        long  l = -8754874239L;
+        float f = 2.2F;
+        double d = 3.14;
+        char a = 'a';
+        boolean  bool = true;
+    }
+}
